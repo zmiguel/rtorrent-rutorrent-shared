dfc910e535a48dbb60bbdd44498148d4c4bd2d54
ZMiguel Valdiviesso <zmiguel@users.noreply.github.com>
2022-01-04 11:31:36 +0000

    Update startup-php.sh

--- startup-php.sh
+++ startup-php.sh
@@ -4,8 +4,8 @@ set -x
 
 MEM=${PHP_MEM:=512M}
 
-sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/8.0/fpm/php.ini
 
 mkdir /run/php
-php-fpm7.4 --nodaemonize
+php-fpm8.0 --nodaemonize
 
