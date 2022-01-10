5fee9d48376a05c7cc5d7d0fabcce89f537313af
ZMiguel Valdiviesso <zmiguel@users.noreply.github.com>
2022-01-10 11:52:26 +0000

    Update startup-rtorrent.sh

--- startup-rtorrent.sh
+++ startup-rtorrent.sh
@@ -11,18 +11,18 @@ groupadd -g $RT_GID rtorrent
 useradd -u $RT_UID -g $RT_GID -d /home/rtorrent -m -s /bin/bash rtorrent
 
 # arrange dirs and configs
-mkdir -p /downloads/.rtorrent/session 
-mkdir -p /downloads/.rtorrent/watch
-mkdir -p /downloads/.log/rtorrent
-if [ ! -e /downloads/.rtorrent/.rtorrent.rc ]; then
-    cp /root/.rtorrent.rc /downloads/.rtorrent/
+mkdir -p /rtorrent/.session 
+mkdir -p /rtorrent/watch
+mkdir -p /rtorrent/log
+if [ ! -e /rtorrent/.rtorrent.rc ]; then
+    cp /root/.rtorrent.rc /rtorrent/
 fi
-ln -s /downloads/.rtorrent/.rtorrent.rc /home/rtorrent/
-chown -R rtorrent:rtorrent /downloads/.rtorrent
+ln -s /rtorrent/.rtorrent.rc /home/rtorrent/
+chown -R rtorrent:rtorrent /rtorrent
 chown -R rtorrent:rtorrent /home/rtorrent
-chown -R rtorrent:rtorrent /downloads/.log/rtorrent
+chown -R rtorrent:rtorrent /rtorrent/log
 
-rm -f /downloads/.rtorrent/session/rtorrent.lock
+rm -f /rtorrent/session/rtorrent.lock
 
 # run
 su --login --command="TERM=xterm rtorrent" rtorrent 
