de4d7a33143799b18cce6969c7d34ce906852629
ZMiguel Valdiviesso <zmiguel@users.noreply.github.com>
2022-01-10 17:22:36 +0000

    Update startup-nginx.sh

--- startup-nginx.sh
+++ startup-nginx.sh
@@ -4,10 +4,10 @@ set -x
 
 chown -R www-data:www-data /var/www/rutorrent
 cp /rtorrent/.htpasswd /var/www/rutorrent/
-mkdir -p /rtorrent/.rutorrent/torrents
-chown -R www-data:www-data /rtorrent/.rutorrent
-mkdir -p /rtorrent/.log/nginx
-chown www-data:www-data /rtorrent/.log/nginx
+mkdir -p /config/rut/torrents
+chown -R www-data:www-data /config/rut
+mkdir -p /config/log/nginx
+chown www-data:www-data /config/log/nginx
 
 rm -f /etc/nginx/sites-enabled/*
 
@@ -20,10 +20,10 @@ rm /var/www/rutorrent/.htpasswd
 site=rutorrent-basic.nginx
 
 # Check if TLS needed
-if [ -e /rtorrent/nginx.key ] && [ -e /rtorrent/nginx.crt ]; then
+if [ -e /config/nginx.key ] && [ -e /config/nginx.crt ]; then
     mkdir -p /etc/nginx/ssl
-    cp /rtorrent/nginx.crt /etc/nginx/ssl/
-    cp /rtorrent/nginx.key /etc/nginx/ssl/
+    cp /config/nginx.crt /etc/nginx/ssl/
+    cp /config/nginx.key /etc/nginx/ssl/
     site=rutorrent-tls.nginx
 fi
 
@@ -32,8 +32,8 @@ cp /root/$site /etc/nginx/sites-enabled/
 [ -n "$WEBROOT" ] && ln -s /var/www/rutorrent /var/www/rutorrent/$WEBROOT
 
 # Check if .htpasswd presents
-if [ -e /rtorrent/.htpasswd ]; then
-    cp /rtorrent/.htpasswd /var/www/rutorrent/ && chmod 755 /var/www/rutorrent/.htpasswd && chown www-data:www-data /var/www/rutorrent/.htpasswd
+if [ -e /config/.htpasswd ]; then
+    cp /config/.htpasswd /var/www/rutorrent/ && chmod 755 /var/www/rutorrent/.htpasswd && chown www-data:www-data /var/www/rutorrent/.htpasswd
 else
 # disable basic auth
     sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/sites-enabled/$site
